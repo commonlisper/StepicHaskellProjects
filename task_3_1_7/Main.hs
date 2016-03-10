d4e3b8de4b390b19f5e86fcebe4e50091a883aa4
Dmytro Pavlenko <atosdo@live.com>
2016-03-10 14:33:03 +0200

    Modify ths solution to the taks 3.1.7 of module 3

--- task_3_1_7/Main.hs
+++ task_3_1_7/Main.hs
@@ -4,12 +4,28 @@ main :: IO ()
 main = do
   print $ "Lists"
   print $ "------------------------------"
-  print $ groupElems []
+  -- print $ groupElems []
   print $ groupElems [1,2]
   print $ groupElems [1,2,2,2,4]
   print $ groupElems [1,2,3,2,4]
   print $ "------------------------------"
 --------------------------------------------------------------------------
 groupElems :: Eq a => [a] -> [[a]]
-groupElems = undefined
+-- groupElems []     = []
+-- groupElems (x:xs) = case xs of
+--   y:ys | y == x -> x:y:groupElems xs
+--   y:ys          -> helper x y ++ groupElems xs
+--   []            -> [x]
+-- --------------------------------------------------------------------------
+-- helper :: [a] -> [a] -> [[a]]
+-- helper (x:_) (y:ys) | x == y = [x,y] ++ helper
+-- helper xs ys = xs : groupWith ys
+-- helper xs [] = [xs]
+--------------------------------------------------------------------------
+groupElems = iter []
+  where iter a [] = reverse a
+        iter [] (x:xs) = iter [[x]] xs
+        iter ((y:ys):yss) (x:xs)
+          | x == y    = iter ((x:y:ys):yss) xs
+          | otherwise = iter ([x]:(y:ys):yss) xs
 --------------------------------------------------------------------------
